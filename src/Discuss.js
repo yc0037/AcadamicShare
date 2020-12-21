3ba602e44b89b31f87600f762c88d9bf891a1b72
Kanae <yzhleave@pku.edu.cn>
2020-12-21 22:22:22 +0800

    Update Discuss.js
    discuss test

--- src/Discuss.js
+++ src/Discuss.js
@@ -1,14 +1,9 @@
-<<<<<<< Updated upstream
 import { Comment, Tooltip, Avatar } from 'antd';
-=======
-import { Comment, Tooltip, Avatar , Input,Form,Button} from 'antd';
->>>>>>> Stashed changes
 import moment from 'moment';
 import { DislikeOutlined, LikeOutlined, DislikeFilled, LikeFilled } from '@ant-design/icons';
 import {List } from 'antd';
 import React  from 'react';
 import { Card } from 'antd';
-<<<<<<< Updated upstream
 
 const data = [
   {
@@ -137,100 +132,5 @@ export default class Paper extends React.Component
       />
     );
     
-=======
-
-const { TextArea } = Input;
-const CommentList = ({ comments }) => 
-(
-  <List
-    dataSource={comments}
-    header={`${comments.length} ${comments.length > 1 ? 'replies' : 'reply'}`}
-    itemLayout="horizontal"
-    renderItem={props => <Comment {...props} />}
-  />
-);
-const Editor = ({ onChange, onSubmit, submitting, value }) => (
-  <div>
-    <Form.Item>
-      <TextArea rows={4} onChange={onChange} value={value} />
-    </Form.Item>
-    <Form.Item>
-      <Button htmlType="submit" loading={submitting} onClick={onSubmit} type="primary">
-        Add Comment
-      </Button>
-    </Form.Item>
-  </div>
-);
-export default  class discussion extends React.Component {
-  
-    constructor(props) {
-    super(props);
-    this.state = {
-      nComment: 0,
-      CommentList: [],
-      hotDiscussList: [],
-      submitting: false,
-      nLike: 0,
-    };
-  }
-  
-  state = 
-  {
-    nComment: [],
-    submitting: false,
-    value: '',
-  };
-  handleSubmit = () => {
-    if (!this.state.value) {
-      return;
-    }
-    this.setState({
-      submitting: true,
-    });
-    setTimeout(() => {
-      this.setState({
-        submitting: false,
-        value: '',
-        comments: [
-          {
-            author: 'Han Solo',
-            avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-            content: <p>{this.state.value}</p>,
-            datetime: moment().fromNow(),
-          },
-          ...this.state.comments,
-        ],
-      });
-    }, 1000);
-  };
-  handleChange = e => {
-    this.setState({
-      value: e.target.value,
-    });
-  };
-  render() {
-    const { comments, submitting, value } = this.state;
-    return (
-      <div>
-        {comments.length > 0 && <CommentList comments={comments} />}
-        <Comment
-          avatar={
-            <Avatar
-              src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-              alt="Han Solo"
-            />
-          }
-          content={
-            <Editor
-              onChange={this.handleChange}
-              onSubmit={this.handleSubmit}
-              submitting={submitting}
-              value={value}
-            />
-          }
-        />
-      </div>
-    );
->>>>>>> Stashed changes
   }
 }
