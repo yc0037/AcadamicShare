7fd65a75d14a62ed7e2158b0253440fa0251f940
Kanae <yzhleave@pku.edu.cn>
2020-12-28 10:21:14 +0800

    重写了讨论界面

--- src/Discuss.js
+++ src/Discuss.js
@@ -1,136 +1,151 @@
-import { Comment, Tooltip, Avatar } from 'antd';
+import {Comment, Tooltip, Avatar, Input, Button, Card, Tag, Space} from 'antd';
 import moment from 'moment';
-import { DislikeOutlined, LikeOutlined, DislikeFilled, LikeFilled } from '@ant-design/icons';
-import {List } from 'antd';
-import React  from 'react';
-import { Card } from 'antd';
+import {DislikeOutlined, LikeOutlined, DislikeFilled, LikeFilled} from '@ant-design/icons';
+import React from 'react';
+import './styles/Discuss.css'
+const dis_url = 'http://localhost:8000/discussion/get_dis?id='
 
-const data = [
-  {
-    actions: [<span key="comment-list-reply-to-0">Reply to</span>],
-    author: 'test',
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    content: (
-      <p>
-        test test test test test test test test test test.
-        test test test test test test test.
-        test test test test test test test test test test.
-      </p>
-    ),
-    datetime: (
-      <Tooltip
-        title={moment()
-          .subtract(1, 'days')
-          .format('YYYY-MM-DD HH:mm:ss')}
-      >
-        <span>
-          {moment()
-            .subtract(1, 'days')
-            .fromNow()}
-        </span>
-      </Tooltip>
-    ),
-  },
-  {
-    actions: [<span key="comment-list-reply-to-0">Reply to</span>],
-    author: ' test',
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    content: (
-      <p>
-        test test test test test test test test test test.
-        test test test test test test test.
-        test test test test test test test test test test.
-      </p>
-    ),
-    datetime: (
-      <Tooltip
-        title={moment()
-          .subtract(2, 'days')
-          .format('YYYY-MM-DD HH:mm:ss')}
-      >
-        <span>
-          {moment()
-            .subtract(2, 'days')
-            .fromNow()}
-        </span>
-      </Tooltip>
-    ),
-  },
-];
+export default class Paper extends React.Component {
+  state =
+    {
+      text: '',
+      comment: {
+        "id": 1,
+        "tags": ["数据库", 'sdfs'],
+        reply_n: 321,
+        creator: 'sfsf',
+        "title": "Hugh Lachlan Kennedy",
+        "time": "2020-11-18T13:14:15+08:00",
+        papers: [{id: 1, name: "Hugh Lachlan Kennedy"}, {id: 2, name: "Hugh Lachlan Kennedy"}],
+        reply: [{
+          id: 12,
+          name: "Hugh Lachlan Kennedy",
+          "time": "2020-11-18T13:14:15+08:00",
+          text: 'Hugh Lachlan Kenned',
+          reconumb: 12
+        }, {
+          name: "Hugh Lachlan Kennedy",
+          "time": "2020-11-18T13:14:15+08:00",
+          text: 'Hugh Lachlan Kenned',
+          reconumb: 12
+        }, {
+          name: "Hugh Lachlan Kennedy",
+          "time": "2020-11-18T13:14:15+08:00",
+          text: 'Hugh Lachlan Kenned',
+          reconumb: 12
+        }]
+      },
+    };
 
-export default class Paper extends React.Component 
-{  
-  
-  state = 
-  {
-    likes: 0,
-    dislikes: 0,
-    action: null,
+  like = (id,number) => {
+    fetch('http://localhost:8000/discussion/reco_up', {
+      method: 'POST', body: JSON.stringify({
+        id,number
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: "include"
+    }).then(res => {
+      this.getComment()
+    })
   };
 
-  like = () => 
-  {
-    this.setState(
-      {
-      likes: 1,
-      dislikes: 0,
-      action: 'liked',
-      });
-  };
+  addComment = (to, text) => {
+    fetch('http://localhost:8000/discussion/reply', {
+      method: 'POST', body: JSON.stringify({
+        to, id:to,text
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: "include"
+    }).then(res => {
+      this.getComment()
+    })
+  }
 
-  dislike = () => {
-    this.setState({
-      likes: 0,
-      dislikes: 1,
-      action: 'disliked',
-    });
-  };
+
+  getComment = () => {
+    fetch(dis_url +  new URLSearchParams(this.props.location.search).get('id'), {
+      credentials: "include"
+    }).then(res => res.json()).then(res => {
+      this.setState({comment: res || {}, text: ''})
+    })
+  }
+
+  componentDidMount() {
+    this.getComment()
+  }
 
   render() {
-    const { likes, dislikes, action } = this.state;
-    const actions = [
-      <span key="comment-basic-like">
-        <Tooltip title="Like">
-          {React.createElement(action === 'liked' ? LikeFilled : LikeOutlined, {
-            onClick: this.like,
-          })}
-        </Tooltip>
-        <span className="comment-action">{likes}</span>
-      </span>,
-      <span key=' key="comment-basic-dislike"'>
-        <Tooltip title="Dislike">
-          {React.createElement(action === 'liked' ? DislikeFilled : DislikeOutlined, {
-            onClick: this.dislike,
-          })}
-        </Tooltip>
-        <span className="comment-action">{dislikes}</span>
-      </span>,
-      <span key="comment-basic-reply-to">Reply to</span>,
-    ];
+    const {comment = {}, text} = this.state;
+    const {papers = [], reply = []} = comment
+    const [question, ...replies] = reply
     return (
-      <Comment
-        actions={actions}
-        author={<a>Han Solo</a>}
-        avatar={
-          <Avatar
-            src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-            alt="Han Solo"
-          />
-        }
-        content={
-          <p>
-            We supply a series of design principles, practical patterns and high quality design
-            resources (Sketch and Axure), to help people create their product prototypes beautifully
-            and efficiently.
-          </p>
-        }
-        datetime={
-          <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
-            <span>{moment().fromNow()}</span>
-          </Tooltip>
-        }
-      />
+      <div style={{padding: 24}}>
+        <Card
+          className={'card'}
+          title={<div><Space>问题描述 {comment.tags && comment.tags.map(tag => <Tag color={'green'}>{tag}</Tag>)}</Space>
+            <Button style={{float: 'right'}} onClick={() => {
+              const ele = document.getElementsByTagName("html")[0];
+              if (ele && ele.scrollHeight > ele.clientHeight) {
+                //设置滚动条到最底部
+                ele.scrollTop = ele.scrollHeight - 200;
+              }
+            }
+            }>发布评论</Button></div>}>
+          <div className={'dis-question'}>{question.text}</div>
+        </Card>
+
+        <Card className={'card'} style={{marginTop: 24}} title={<span>{comment.reply_n} 问题回复</span>}>
+          <div className={'discuss-content'}>{replies.map((item, index) => (
+            <div className={'dis-reply'}>
+              <div className={'flex-row'}>
+                <Avatar
+                  src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                />
+                {item.name}
+                <div style={{flex: 1}}></div>
+                <span style={{color: 'grey'}}>{moment(item.time).format('YYYY-MM-DD HH:mm:ss')}</span>
+              </div>
+              <div style={{margin: '24px 0'}}>{item.text}</div>
+              <div>
+                <Tooltip title="Like">
+                  {React.createElement(LikeOutlined, {
+                    onClick: () => this.like(comment.id,index+1),
+                  })}
+                </Tooltip>
+                <span className="comment-action">{item.reconumb}</span>
+              </div>
+            </div>
+          ))}
+          </div>
+        </Card>
+
+
+        <div className={'card'} style={{marginTop: 24, padding: '16px 16px 60px 16px'}}>
+          <div className={'discuss-add'}>
+            <Avatar
+              src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+            />
+            can you contribute to the discuss？
+          </div>
+          <div style={{marginTop: 16}}>
+            <Input.TextArea value={text} onChange={e => this.setState({text: e.target.value})}/><br/>
+            <Button type={'primary'} style={{marginTop: 16, float: 'right'}}
+                    onClick={() => this.addComment(comment.id, text)}>Add Comment</Button>
+          </div>
+        </div>
+        <Card
+          className={'card'}
+          style={{marginTop: 24}}
+          title='相关论文'>
+          {papers.filter(i=>i).map(item => <div className={'dis-question'}><Tag>{item.id}</Tag>{item.name}</div>)}
+        </Card>
+      </div>
     );
-    
+
   }
+
 }
